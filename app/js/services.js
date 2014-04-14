6c081387fe348bfad0f73920e97e42cfe1d4b172
Moreno <moreno.pinheiro@gmail.com>
2014-04-13 21:45:13 -0300

    setup angular pounchdb installation

--- app/js/services.js
+++ app/js/services.js
@@ -7,3 +7,10 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1');
+
+// Inject the pouchdb object into your services
+// See: https://github.com/wspringer/angular-pouchdb#usage
+// angular.module('myApp.services')
+//   .factory('$db', function(pouchdb){
+//     // Do something with pouchdb.
+//   });
